a680583e9eca3700704a790b6f70756045c9e36a
kanglerian <kanglerian@gmail.com>
2022-03-29 19:22:43 +0700

    add website

--- js/Object/biodata.js
+++ js/Object/biodata.js
@@ -4,8 +4,14 @@ let person = {
     title: "A.Md.Kom",
     age: 24,
     role: "Informatics Engineer",
-    based: "Kota Tasikmalaya, Indonesia"
+    based: "Kota Tasikmalaya, Indonesia",
+    location: "🇮🇩 Kota Tasikmalaya, Indonesia",
+    website: "kanglerian.github.io",
+    github: "github.com/kanglerian"
 }
 document.getElementById("nama").innerHTML = person.firstName + " " + person.lastName + ", " + person.title;
 document.getElementById("role").innerHTML = person.role;
 document.getElementById("based").innerHTML = person.based;
+document.getElementById("location").innerHTML = person.location;
+document.getElementById("website").innerHTML = person.website;
+document.getElementById("github").innerHTML = person.github;
