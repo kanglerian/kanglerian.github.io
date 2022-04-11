a3174695a9e087806a4cbc4ec698ca6cc2a2722f
kampusinformatika <kanglerian.business@gmail.com>
2022-04-11 15:09:46 +0700

    add aos

---
+++ js/gsap.js
@@ -0,0 +1,5 @@
+gsap.from(".header-title", { duration: 2.5, ease: "elastic.out(1, 0.3)", y: -500, rotation: 25 });
+gsap.from(".underline", { duration: 2.5, ease: "elastic.out(1, 0.3)", y: -500, rotation: 25, delay:0.1 });
+gsap.from(".header-desc", { duration: 2.5, ease: "elastic.out(1, 0.3)", y: -600, rotation: 25, delay:0.2 });
+gsap.from(".nav-link", { duration: 2.5, ease: "elastic.out(1, 0.3)", scale: 0, rotation: 25, delay:0.3 });
+gsap.from(".social-media", { duration: 2.5, ease: "elastic.out(1, 0.3)", scale: 0, rotation: 25, delay:1 });
