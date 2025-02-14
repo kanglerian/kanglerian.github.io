547251d41cb02319b88181ace40bce42f5cac2a8
Lerian Febriana <kanglerian@gmail.com>
2025-02-14 15:22:55 +0700

    animate

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ import laptopCodeIcon from './assets/icons/laptop-code.svg'
 import DatabaseJSON from './assets/data/database.json'
 import DetailPortofolio from './components/DetailPortofolio'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { gsap } from 'gsap';
 
 const App = () => {
+
   const [projects, setProjects] = useState([]);
   const [tools, setTools] = useState([]);
 
@@ -26,34 +28,112 @@ const App = () => {
 
   useEffect(() => {
     getDatabase();
+    gsap.from('#profile', {
+      y: -500,
+      rotate: 150,
+    });
+    gsap.from('#profile-name', {
+      y: -800,
+    });
+    gsap.from('#profile-desc', {
+      opacity: 0,
+      y: -800,
+    });
+    gsap.from('#link-1', {
+      y: -800,
+      rotate: 150,
+    });
+    gsap.from('#link-2', {
+      y: -800,
+      rotate: 150,
+    });
+    gsap.from('#link-3', {
+      y: -800,
+      rotate: 150,
+    });
+    gsap.from('#link-4', {
+      y: -800,
+      rotate: 150,
+    });
+    gsap.to('#profile', {
+      duration: 3.5,
+      rotate: 0,
+      y: 0,
+      ease: "elastic.out(1,0.2)"
+    });
+    gsap.to('#profile-name', {
+      duration: 3.5,
+      delay: 0.2,
+      y: 0,
+      ease: "elastic.out(1,0.2)"
+    });
+    gsap.to('#profile-desc', {
+      duration: 3.5,
+      delay: 0.3,
+      opacity: 1,
+      y: 0,
+      ease: "elastic.out(1,0.2)"
+    });
+    gsap.to('#link-1', {
+      duration: 3.5,
+      delay: 0.6,
+      rotate: 0,
+      y: 0,
+      ease: "elastic.out(1,0.2)"
+    });
+    gsap.to('#link-2', {
+      duration: 3.5,
+      delay: 0.7,
+      rotate: 0,
+      y: 0,
+      ease: "elastic.out(1,0.2)"
+    });
+    gsap.to('#link-3', {
+      duration: 3.5,
+      delay: 0.8,
+      rotate: 0,
+      y: 0,
+      ease: "elastic.out(1,0.2)"
+    });
+    gsap.to('#link-4', {
+      duration: 3.5,
+      delay: 0.9,
+      rotate: 0,
+      y: 0,
+      ease: "elastic.out(1,0.2)"
+    });
   }, []);
+
   return (
     <main>
       <section className="flex flex-col justify-center items-center bg-slate-950 h-screen">
         <header className='flex flex-col justify-center items-center gap-5'>
-          <img loading="lazy" src={ManImage} alt="Lerian Febriana, A.Md.Kom" className='w-32 h-32' />
+          <img id='profile' loading="lazy" src={ManImage} alt="Lerian Febriana, A.Md.Kom" className='w-32 h-32' />
           <div className='max-w-xl flex flex-col justify-center gap-2 text-center px-5 md:px-0'>
-            <h1 className='text-white font-bold text-xl'>Lerian Febriana, A.Md.Kom</h1>
-            <p className='text-gray-200 text-sm'>🌿 Hi there! I&apos;m Lerian, a nature enthusiast and IT enthusiast from Indonesia. I&apos;m passionate about digging into tech and aiming to become an IT expert.</p>
-            <p className='text-gray-200 text-sm'>Indonesian 🇮🇩</p>
+            <h1 id='profile-name' className='text-white font-bold text-xl'>Lerian Febriana, A.Md.Kom</h1>
+            <p id='profile-desc' className='text-gray-200 text-sm'>🌿 Hi there! I&apos;m Lerian, a nature enthusiast and IT enthusiast from Indonesia. I&apos;m passionate about digging into tech and aiming to become an IT expert.<br/>Indonesian 🇮🇩</p>
           </div>
           <section className='flex flex-wrap justify-center items-center gap-2'>
-            <a href="https://github.com/kanglerian" target='_blank' className='inline-block text-sm text-white hover:text-black hover:bg-white border border-gray-200 px-5 py-2 space-x-1'>
+            <a id='link-1' href="https://github.com/kanglerian" target='_blank' className='inline-block text-sm text-white hover:text-black hover:bg-white border border-gray-200 px-5 py-2 space-x-1'>
               <FontAwesomeIcon icon="fa-brands fa-github" size='md' />
               <span>Github</span>
             </a>
-            <a href="https://youtube.com/c/kanglerian" target='_blank' className='inline-block text-sm text-white hover:text-black hover:bg-white border border-gray-200 px-5 py-2 space-x-1'>
+            <a id='link-2' href="https://youtube.com/c/kanglerian" target='_blank' className='inline-block text-sm text-white hover:text-black hover:bg-white border border-gray-200 px-5 py-2 space-x-1'>
               <FontAwesomeIcon icon="fa-brands fa-youtube" size='md' />
               <span>Youtube</span>
             </a>
-            <a href="https://www.linkedin.com/in/kanglerian" target='_blank' className='inline-block text-sm text-white hover:text-black hover:bg-white border border-gray-200 px-5 py-2 space-x-1'>
+            <a id='link-3' href="https://manabispace.blogspot.com" target='_blank' className='inline-block text-sm text-white hover:text-black hover:bg-white border border-gray-200 px-5 py-2 space-x-1'>
+              <FontAwesomeIcon icon="fa-brands fa-blogger-b" size='md' />
+              <span>Blogger</span>
+            </a>
+            <a id='link-4' href="https://www.linkedin.com/in/kanglerian" target='_blank' className='inline-block text-sm text-white hover:text-black hover:bg-white border border-gray-200 px-5 py-2 space-x-1'>
               <FontAwesomeIcon icon="fa-brands fa-linkedin" size='md' />
               <span>Linkedin</span>
             </a>
           </section>
         </header>
       </section>
-      <section className='bg-slate-900'>
+      {/* <section className='bg-slate-900'>
         <div className='container mx-auto space-y-10 px-10 py-10 md:px-5'>
           {
             tools.length > 0 ? (
@@ -71,8 +151,8 @@ const App = () => {
             )
           }
         </div>
-      </section>
-      <section className='bg-slate-950'>
+      </section> */}
+      {/* <section className='bg-slate-950'>
         <div className='container mx-auto space-y-10 px-10 py-10 md:px-5'>
           <header className='text-center'>
             <h2 className='flex justify-center items-center gap-3 text-2xl font-bold'>
@@ -116,9 +196,9 @@ const App = () => {
             )
           }
         </div>
-      </section>
+      </section> */}
       <footer className='py-3 bg-slate-900'>
-        <p className='text-xs text-center text-gray-300'>Copyright © 2024 Lerian Febriana 🇮🇩</p>
+        <p className='text-xs text-center text-gray-300'>Copyright © 2024 Lerian Febriana. All rights reserved.</p>
       </footer>
     </main>
   )
